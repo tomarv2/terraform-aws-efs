9e5e5b5483229f50fe4986f307d03948d2cb1bca
tomarv2 <varuntomar2019@gmail.com>
2021-03-22 15:50:25 -0700

    removing extra tfvars file and updating output file

--- examples/output.tf
+++ examples/output.tf
@@ -1,23 +1,11 @@
-//output "efs_name" {
-//  value = var.efs_name
-//}
+output "file_system_id" {
+  value = module.efs.file_system_id
+}
 
-//output "file_system_id" {
-//  value = aws_efs_file_system.efs.id
-//}
-//
-//output "dns_name" {
-//  value = aws_efs_file_system.efs.dns_name
-//}
-//
-//output "mount_target_ids" {
-//  value = join(",", aws_efs_mount_target.efs.*.id)
-//}
-//
-//output "mount_target_interface_ids" {
-//  value = join(",", aws_efs_mount_target.efs.*.network_interface_id)
-//}
-//
-//output "security_group_id" {
-//  value = aws_security_group.efs.id
-//}
+output "file_system_dns_name" {
+  value = module.efs.file_system_dns_name
+}
+
+output "file_system_arn" {
+  value = module.efs.file_system_arn
+}
--- output.tf
+++ output.tf
@@ -2,14 +2,10 @@ output "file_system_id" {
   value = join("", aws_efs_file_system.efs.*.id)
 }
 
-output "dns_name" {
+output "file_system_dns_name" {
   value = join("", aws_efs_file_system.efs.*.dns_name)
 }
 
-//output "mount_target_ids" {
-//  value = join(",", aws_efs_mount_target.efs.*.id)
-//}
-//
-//output "mount_target_interface_ids" {
-//  value = join(",", aws_efs_mount_target.efs.*.network_interface_id)
-//}
+output "file_system_arn" {
+  value = join("", aws_efs_file_system.efs.*.arn)
+}
